3618b1390f4b36bcef27ead4827761ad0c9922a9
woynert <woynertgamer@gmail.com>
2024-02-25 19:29:12 -0500

    feat: update context properties + setup methods

--- aboutlife/context.py
+++ aboutlife/context.py
@@ -1,11 +1,15 @@
+import time
 import threading
 from enum import Enum
 
+TOMATO_BREAK_DURATION = 6
 
-class State(Enum):
-    IDLE = 0  # overlay shown
-    RESTING = 1  # overlay shown, countdown
-    WORKING = 2  # countdown
+
+class STATE(Enum):
+    IDLE = 0  # overlay shows
+    OBLIGATORY_BREAK = 1  # overlay shows
+    TOMATO_BREAK = 2  # overlay shows
+    WORKING = 3  # floating message shows
 
 
 class Context:
@@ -14,16 +18,17 @@ class Context:
     _singleton = None
 
     def __init__(self):
-        self.state: State = State.IDLE
-        self.prev_state: State = self.state
-        self.time_start: int = 0
-        self.time_end: int = 0
+        self.state: STATE = STATE.IDLE
+        self.close_tabs: bool = False
+        self.end_time: int = 0  # unix time from system
+        self.task_info: str = ""
+        self.reset()
 
     def reset(self):
-        self.state = State.IDLE
-        self.prev_state: State = self.state
-        self.time_start = 0
-        self.time_end = 0
+        self.state = STATE.IDLE
+        self.close_tabs = False
+        self.end_time = 0
+        self.task_info = "Sample Task Info"
 
     @classmethod
     def get_singleton(cls) -> "Context":
@@ -34,3 +39,26 @@ class Context:
     @classmethod
     def get_mutex(cls) -> threading.Lock:
         return cls._mutex
+
+    def setup_tomato_break(self) -> bool:
+        if self.state != STATE.IDLE:
+            return False
+
+        self.state = STATE.TOMATO_BREAK
+        self.end_time = int(time.time()) + TOMATO_BREAK_DURATION * 60
+        return True
+
+    def setup_work_session(self, task_info: str, duration: int) -> bool:
+        if not (
+            duration > 0
+            and duration <= 30
+            and len(task_info) > 0
+            and len(task_info) <= 40
+            and self.state == STATE.IDLE
+        ):
+            return False
+
+        self.state = STATE.WORKING
+        self.task_info = task_info
+        self.end_time = int(time.time()) + duration * 60
+        return True
