0b852cf52438a1aed77f1f13b4b9f550bbd0a33b
woynert <woynertgamer@gmail.com>
2024-02-27 01:23:13 -0500

    feat(tray): option to end session prematurely

--- aboutlife/tray/tray.py
+++ aboutlife/tray/tray.py
@@ -1,4 +1,5 @@
 from aboutlife.plugin import Plugin
+from aboutlife.context import Context
 import gi
 
 gi.require_version("Gtk", "3.0")
@@ -16,14 +17,9 @@ class TrayPlugin(Plugin):
         self.tray_icon.connect("activate", lambda x: print("Hello"))
 
         self.menu = Gtk.Menu()
-        # end button
-        item_end = Gtk.MenuItem(label="Terminar")
-        item_end.connect("activate", Gtk.main_quit)
-        self.menu.append(item_end)
-        # interrupt button
-        item_interrupt = Gtk.MenuItem(label="Interrumpir")
-        item_interrupt.connect("activate", Gtk.main_quit)
-        self.menu.append(item_interrupt)
+        item = Gtk.MenuItem(label="Terminar")
+        item.connect("activate", self.on_finish)
+        self.menu.append(item)
         self.menu.show_all()
 
         self.tray_icon.set_tooltip_text("Está bien :)")
@@ -39,6 +35,10 @@ class TrayPlugin(Plugin):
 
     def open_menu(self, status_icon, button, time):
         self.menu.popup(None, None, None, status_icon, button, time)
+
+    def on_finish(self, widget):
+        with Context.get_mutex():
+            Context.get_singleton().setup_obligatory_break()
 
 
 if __name__ == "__main__":
